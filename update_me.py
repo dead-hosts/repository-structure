fbb438fc4fe8650287e0f79f43f1f376ca5b419b
funilrys <contact@funilrys.com>
2018-06-30 19:20:36 +0200

    Let's find why the system is not pushing'

--- update_me.py
+++ update_me.py
@@ -543,11 +543,13 @@ class Initiate(object):
             Helpers.Dict(Settings.informations).to_json(Settings.repository_info)
             self.travis_permissions()
 
-            Helpers.Command(
-                "git add --all && git commit -a -m '%s' && git push origin %s"
-                % (commit_message, environ["GIT_BRANCH"]),
-                False,
-            ).execute()
+            print(
+                Helpers.Command(
+                    "git add --all && git commit -a -m '%s' && git push origin %s"
+                    % (commit_message, environ["GIT_BRANCH"]),
+                    True,
+                ).execute()
+            )
         else:
             print(
                 "No need to test until %s."
